3d851ce776bb1dd83329643810a4a86a4fa82a19
juan.cantero <juan.cantero@outlook.com>
2025-11-07 17:41:28 -0300

    Initial commit: Go breeders application
    - Add basic Go module setup with go.mod and go.sum
    - Add template files for all pages (home, about, breeds, breeders)
    - Add base layout template with Bootstrap navigation
    - Add .gitignore for Go projects

---
+++ cmd/web/handlers.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
+	app.render(w, "home.page.gohtml", nil)
+
+}
---
+++ cmd/web/render.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type temmplateData struct {
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, t string, td *temmplateData) {
+	var tmpl *template.Template
+
+	// template cache, try to get the template from our map , stored in the receiver
+	if app.config.useCache {
+		if templateFromMap, ok := app.templateMap[t]; ok {
+			tmpl = templateFromMap
+		}
+	}
+
+	if tmpl == nil {
+		newTemplate, err := app.buildTemplateFromDisk(t)
+		if err != nil {
+			log.Println("error building template", err)
+			return
+		}
+		log.Println("building template from disk")
+		tmpl = newTemplate
+	}
+
+	if td == nil {
+		td = &temmplateData{}
+	}
+
+	if err := tmpl.ExecuteTemplate(w, t, td); err != nil {
+		log.Println("error executing template", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
+func (app *application) buildTemplateFromDisk(t string) (*template.Template, error) {
+	templateSlice := []string{
+		"./templates/base.layout.gohtml",
+		"./templates/partials/header.partial.gohtml",
+		"./templates/partials/footer.partial.gohtml",
+		fmt.Sprintf("./templates/%s", t),
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	app.templateMap[t] = tmpl
+
+	return tmpl, nil
+
+}
